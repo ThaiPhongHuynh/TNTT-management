012f6a15b57489100c2fecd005ae3f2c74b0b44f
ThaiPhong1502 <66869577+ThaiPhong1502@users.noreply.github.com>
2021-08-04 16:22:22 +0700

    setup2
    setup-2

---
+++ Models/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tntt_manegement.Models
+{
+    public class Teacher
+    {
+        public int teacherId { get; set; }
+        public string userName { get; set; }
+        public string holyName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+
+        public Group Group { get; set; }
+        public Class Class { get; set; }
+
+        public int groupId { get; set; }
+        public int classId { get; set; }
+    }
+}
